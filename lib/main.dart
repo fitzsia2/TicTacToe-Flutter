d3e280dba59089a63b786e4a7ae6e6a625e13a27
Andrew Fitzsimons <andrewsfitzsimons@gmail.com>
2020-10-29 13:38:34 +0100

    Refactor Counter

--- lib/main.dart
+++ lib/main.dart
@@ -43,12 +43,13 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: CounterDisplay(count: _counter),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Counter(title: 'Counter 1'),
+            Counter(title: 'Counter 2'),
+          ],
+        ),
       ),
     );
   }
@@ -65,14 +66,61 @@ class CounterDisplay extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Text(
-            'You have pushed the button this many times:',
+            'Count: $count',
+            style: Theme.of(context).textTheme.bodyText2,
           ),
-          Text(
-            '$count',
-            style: Theme.of(context).textTheme.headline4,
-          ),
-        ]
-    );
+        ]);
   }
 }
 
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({Key key, this.onPressed}) : super(key: key);
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: onPressed, child: Icon(Icons.add));
+  }
+}
+
+class Counter extends StatefulWidget {
+  Counter({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          '${widget.title}',
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CounterIncrementor(onPressed: _increment),
+            SizedBox(
+              width: 8.0,
+            ),
+            CounterDisplay(count: _counter),
+          ],
+        ),
+      ],
+    );
+  }
+}
